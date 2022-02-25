b180ae58fa0038dfa523103ef3ba7e4fdd602969
Ade <shennaike2386@hotmail.com>
2022-02-25 12:51:40 -0500

    created an axios with authorization

--- frontend/axios/index.js
+++ frontend/axios/index.js
@@ -1,1 +1,13 @@
 // ✨ implement axiosWithAuth
+import axios from 'axios';
+
+export default function axiosWithAuth() {
+    const token = localStorage.getItem('token')
+
+    axios.create({
+        headers: {
+            Authorization: token
+        },
+        baseURL: 'http://localhost:9000/api/articles'
+    })
+}
